13034ba381166a50175f42314a64c19da00bb4ae
Red Pymes <clientesredpymes@gmail.com>
2026-09-18 20:15:54 -0500

    fix: typing of modules array in medusa-config (avoid TS2353 between notification and file-s3 providers)

--- medusa-marketplace-demo/medusa-config.ts
+++ medusa-marketplace-demo/medusa-config.ts
@@ -27,33 +27,34 @@ const modules = [
       ],
     },
   },
-]
-
-if (s3Configured) {
-  modules.push({
-    resolve: "@medusajs/medusa/file",
-    options: {
-      providers: [
+  ...(s3Configured
+    ? [
         {
-          resolve: "@medusajs/medusa/file-s3",
-          id: "s3",
+          resolve: "@medusajs/medusa/file",
           options: {
-            file_url: process.env.S3_FILE_URL,
-            access_key_id: process.env.S3_ACCESS_KEY_ID,
-            secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-            region: process.env.S3_REGION || "auto",
-            bucket: process.env.S3_BUCKET,
-            endpoint: process.env.S3_ENDPOINT,
-            cache_control: "public, max-age=31536000",
-            additional_client_config: {
-              forcePathStyle: true,
-            },
+            providers: [
+              {
+                resolve: "@medusajs/medusa/file-s3",
+                id: "s3",
+                options: {
+                  file_url: process.env.S3_FILE_URL,
+                  access_key_id: process.env.S3_ACCESS_KEY_ID,
+                  secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                  region: process.env.S3_REGION || "auto",
+                  bucket: process.env.S3_BUCKET,
+                  endpoint: process.env.S3_ENDPOINT,
+                  cache_control: "public, max-age=31536000",
+                  additional_client_config: {
+                    forcePathStyle: true,
+                  },
+                },
+              },
+            ],
           },
         },
-      ],
-    },
-  })
-}
+      ]
+    : []),
+]
 
 module.exports = defineConfig({
   admin: {
